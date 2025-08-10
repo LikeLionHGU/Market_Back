2fd1693f3907d2e2c0e24aca49960aca8884d7de
7SH7 <kimseunghwan7777@gmail.com>
2025-08-10 23:08:12 +0900

    ✏️ Docs: add simple explaination
    yml에서 기본적으로 /api 붙여주는 거 설명 주석 추가함.

--- src/main/java/org/example/marketback/controller/MainPageController.java
+++ src/main/java/org/example/marketback/controller/MainPageController.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/main")
+@RequestMapping("/main")  // yml에 기본적으로 /api 붙여주니까, 참고해서 작업해
 public class MainPageController {
 }

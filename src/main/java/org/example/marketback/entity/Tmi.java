2f676821f84ce35eae7c579b822ea0732fd802a6
7SH7 <kimseunghwan7777@gmail.com>
2025-08-11 00:05:17 +0900

    🎨 Chore: add entity variable

--- src/main/java/org/example/marketback/entity/Tmi.java
+++ src/main/java/org/example/marketback/entity/Tmi.java
@@ -16,7 +16,8 @@ public class Tmi extends AuditingFields {
     private String content;
     private Enum category;  // todo: add category enum
     private String email; // todo: email 보내는 건 나중에 해도 됨. (우선순위 낮아.) -> 제일 마지막
-    private Long like;
+    private Long likes;
+    private Long views;
     
     @ManyToOne
     @JoinColumn(name = "market_id")

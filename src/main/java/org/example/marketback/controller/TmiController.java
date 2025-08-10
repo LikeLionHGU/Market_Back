377d01b1c07ff25da3ddba117874a230e65f4816
7SH7 <kimseunghwan7777@gmail.com>
2025-08-10 21:59:04 +0900

    ✨ Feat: set controller file

---
+++ src/main/java/org/example/marketback/controller/TmiController.java
@@ -0,0 +1,11 @@
+package org.example.marketback.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/tmi")
+public class TmiController {
+}

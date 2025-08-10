68820f176132deeac9b13f73362649546ed9f2af
7SH7 <kimseunghwan7777@gmail.com>
2025-08-10 19:59:04 +0900

    📝 Chore: change yml file option
    yml 파일 설정 변경

--- src/main/java/org/example/marketback/MarketBackApplication.java
+++ src/main/java/org/example/marketback/MarketBackApplication.java
@@ -2,8 +2,12 @@ package org.example.marketback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableScheduling  // 스케줄링을 위해 (특정 시간에 자동으로 TMI + history 바꿔야하니까)
 public class MarketBackApplication {
 
     public static void main(String[] args) {

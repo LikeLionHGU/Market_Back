bd76bc8e436d6014a0b0daad2575c6cb17c44983
7SH7 <kimseunghwan7777@gmail.com>
2025-08-10 23:51:38 +0900

    🎨 Chore: set structure(service)

---
+++ src/main/java/org/example/marketback/service/impl/MainPageServiceImpl.java
@@ -0,0 +1,10 @@
+package org.example.marketback.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.example.marketback.service.MainPageService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MainPageServiceImpl implements MainPageService {
+}
---
+++ src/main/java/org/example/marketback/service/impl/TmiServiceImpl.java
@@ -0,0 +1,10 @@
+package org.example.marketback.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.example.marketback.service.TmiService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TmiServiceImpl implements TmiService {
+}
